030ea36c3b6a5de6e9000e1c5866e1616d27c840
Phi Luu <philectron@users.noreply.github.com>
2018-11-28 11:41:03 -0800

    Draw a red box inside a blue screen

--- lab6_video_graphics_array_vga/lab6/VgaDrawer.sv
+++ lab6_video_graphics_array_vga/lab6/VgaDrawer.sv
@@ -1,4 +1,9 @@
-module VgaDrawer(
+module VgaDrawer#(parameter
+    HACTIVE = 10'd640,                    // horizontal active resolution
+    VACTIVE = 10'd480,                    // verical active resolution
+    BOXW = 10'd320,                       // box width
+    BOXH = 10'd240                        // box height
+)(
     input logic clk,
     input logic isdisplayed,
     input logic [9:0] x, y,
@@ -9,7 +14,11 @@ logic [3:0] r_out = 0, g_out = 0, b_out = 0;
 
 always_ff @(posedge clk)
     if (isdisplayed)
-        {r_out, g_out, b_out} <= {4'b0, 4'b0, 4'b1111};
+        if ((HACTIVE - BOXW) / 2 <= x && x < (HACTIVE - BOXW) / 2 + BOXW
+            && (VACTIVE - BOXH) / 2 <= y && y < (VACTIVE - BOXH) / 2 + BOXH)
+            {r_out, g_out, b_out} <= {4'b1111, 4'b0, 4'b0};
+        else
+            {r_out, g_out, b_out} <= {4'b0, 4'b0, 4'b1111};
     else
         {r_out, g_out, b_out} <= {4'b0, 4'b0, 4'b0};
 
