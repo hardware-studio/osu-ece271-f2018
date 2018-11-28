df74d4eb8a96ea775d5d83f18863509dce7086c8
Phi Luu <philectron@users.noreply.github.com>
2018-11-28 01:35:25 -0800

    Make screen brighter

--- lab6_video_graphics_array_vga/lab6/VgaDrawer.sv
+++ lab6_video_graphics_array_vga/lab6/VgaDrawer.sv
@@ -9,7 +9,7 @@ logic [3:0] r_out = 0, g_out = 0, b_out = 0;
 
 always_ff @(posedge clk)
     if (isdisplayed)
-        {r_out, g_out, b_out} <= {4'b0, 4'b1, 4'b0};
+        {r_out, g_out, b_out} <= {4'b0, 4'b0, 4'b1111};
     else
         {r_out, g_out, b_out} <= {4'b0, 4'b0, 4'b0};
 
