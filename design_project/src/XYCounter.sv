62e70e1f2f1ce48054d3c7e61d00d5bdd53d07b1
Phi Luu <philectron@users.noreply.github.com>
2018-11-30 05:30:42 -0800

    Adjust the starting and ending points of sync pulses

--- design_project/src/XYCounter.sv
+++ design_project/src/XYCounter.sv
@@ -4,14 +4,14 @@ module XYCounter(input logic clk,
 	// Track current x and y
 	logic[9:0] xpos = 0;
 	logic[9:0] ypos = 0;
-	
+
 	always_ff @(posedge clk)
 		begin
 			// Generate HSync and VSync signals when in sync section
-			hs <= ~((xpos > 640 + 16) && (xpos < 640 + 16 + 96));
-			vs <= ~((ypos > 480 + 11) && (ypos < 480 + 11 + 2));
-			
-			if(xpos == 800 && ypos == 524)	// Reset when at end of screen
+			hs <= ~((xpos >= 640 + 16) && (xpos < 640 + 16 + 96));
+			vs <= ~((ypos >= 480 + 10) && (ypos < 480 + 10 + 2));
+
+			if(xpos == 800 && ypos == 525)	// Reset when at end of screen
 				begin
 					ypos <= 0;
 					xpos <= 0;
@@ -21,11 +21,11 @@ module XYCounter(input logic clk,
 					ypos <= ypos + 1;
 					xpos <= 0;
 				end
-			else	// Otherwise increment x 
+			else	// Otherwise increment x
 				xpos <= xpos + 1;
 		end
-		
+
 	assign x = xpos;
 	assign y = ypos;
-		
+
 endmodule
